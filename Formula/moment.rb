d5ad8b643e84a6c89ad6b8458b116555648888f1
Pete Yandell <pete@notahat.com>
2026-03-14 15:24:13 +1100

    Fix tarball URL and SHA256

--- Formula/moment.rb
+++ Formula/moment.rb
@@ -1,8 +1,8 @@
 class Moment < Formula
   desc "Command-line tool to list calendar events and reminders for the next 7 days"
   homepage "https://github.com/notahat/moment"
-  url "https://github.com/notahat/moment/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  url "https://github.com/notahat/moment/archive/v0.1.0.tar.gz"
+  sha256 "21e24cf4ce76d52c3491f632ee2cbda888677d999b6a9875523c7a43e936af88"
   license "MIT"
 
   depends_on xcode: ["16.0", :build]
